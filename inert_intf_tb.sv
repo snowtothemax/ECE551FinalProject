91356e26657c09908d86d9f2e3722ee288f9d70c
Ranjan5 <76456333+Ranjan5@users.noreply.github.com>
2021-04-07 09:59:51 -0500

    Add files via upload

--- inert_intf_tb.sv
+++ inert_intf_tb.sv
@@ -18,14 +18,50 @@ module inert_intf_tb();
 		// Reset the DUT - Don't Assert start_cal yet! //
 		rst_n = 0;
 		clk = 0;
-		// Wait for NEMO setup using the fork/join technique where we wait for _ to assert otherwise timeout //
-		
+
+		repeat(2) @(posedge clk);
+
+		rst_n = 1;
+		// Wait for NEMO setup using the fork/join technique where we wait for INT to assert otherwise timeout //
+		fork 
+			begin: timeout1
+				repeat(1000000) @(posedge clk);
+				$display("timed out (INT)!")
+				$stop
+			end
+			begin
+				@(posedge INT);
+				disable timeout1;
+			end
+		join
+
 		// Assert strt_cal //
+
+		strt_cal = 1;
+
+		repeat(2) @(posedge clk);
+
+		strt_cal = 0;
 		
 		// Wait for cal_done - timeout loop should be ~1mil clk cycles //
+
+		fork 
+			begin: timeout2
+				repeat(1000000) @(posedge clk);
+				$display("timed out (cal_done)!")
+				$stop
+			end
+			begin
+				@(posedge cal_done);
+				disable timeout2;
+			end
+		join
 		
 		// Let it run for like 8mil clock cycles and plot ptch, roll, yaw in ModelSim as an analog signal to observe waveform //
-		
+		repeat(8000000) @(posedge clk);
+
+		$display("Done!");
+		$stop;
 	end
 	
 	
