057facacc4138f0e11abdd88bd122faba923aac1
bschisel <49885883+Fellaini2427@users.noreply.github.com>
2021-04-12 10:28:12 -0500

    cmd_cfg update

--- cmd_cfg.sv
+++ cmd_cfg.sv
@@ -15,29 +15,62 @@ module cmd_cfg(clk, rst_n, cmd_rdy, cmd, data, clr_cmd_rdy, resp, send_resp, d_p
 	output motors_off;							// Shuts off motors
 	
 	// Declare internal signals //
-	
+	parameter FAST_SIM = 1;
+	logic [25:0] timer;
 	
 	// Declare SM signals //
 	typedef enum reg [2:0] {IDLE, } state_t;
 	state_t state, nxt_state;
 	
 	// d_ptch register //
-	
+	always_ff @(posedge clk, negedge rst_n)begin
+		if (~rst_n)
+			d_ptch <= 16'h0;
+		else if (wptch)
+			d_ptch <= data;
+	end
 	
 	// d_roll register //
-	
+	always_ff @(posedge clk, negedge rst_n)begin
+		if (~rst_n)
+			d_roll <= 16'h0;
+		else if (wroll)
+			d_roll <= data;
+	end
 	
 	// d_yaw register //
-	
+	always_ff @(posedge clk, negedge rst_n)begin
+		if (~rst_n)
+			d_yaw <= 16'h0;
+		else if (wyaw)
+			d_yaw <= data;
+	end
 	
 	// thrst register //
-	
+	always_ff @(posedge clk, negedge rst_n)begin
+		if (~rst_n)
+			thrst <= 16'h0;
+		else if (wthrst)
+			thrst <= data;
+	end
 	
 	// Variable Width Timer //
-	
+	always_ff @(posedge clk, negedge rst_n) begin
+		if (~rst_n)
+			timer <= 26'h0;
+		else
+			timer <= timer + 1'b1;
+	end
 	
 	// Output flop & logic //
-	
+	always_ff @(posedge clk, negedge rst_n) begin
+		if (~rst_n)
+			motors_off <= 1'b1;
+		else if (mtrs_off)
+			motors_off <= 1'b1;
+		else if (inertial_cal)
+			motors_off <= 1'b0;
+	end
 	
 	// SM //
 	
